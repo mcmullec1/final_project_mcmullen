16fe9ad2b38bcdac6fe30868017996e8b54db07c
clare-mac <mcmclare011@gmail.com>
2024-05-04 21:08:06 +0100

    changing base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://mcmullec1.github.io/final_project_mcmullen/'
+  //base:'https://mcmullec1.github.io/final_project_mcmullen/'
+  base:'/final_project_mcmullen/'
 })
